12dc0574889153c6d84aa21816be9c8f93e92e60
Howard <howard.tzw@gmail.com>
2022-08-03 10:11:02 +0800

    add: connect google spreadsheet data

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,3 +3,28 @@ config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
 export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
+export const {
+  TYPE,
+  PROJECT_ID,
+  PRIVATE_KEY_ID,
+  PRIVATE_KEY,
+  CLIENT_EMAIL,
+  CLIENT_ID,
+  AUTH_URI,
+  TOKEN_URI,
+  AUTH_PROVIDER_X509_CERT_URL,
+  CLIENT_X509_CERT_URL,
+} = process.env;
+
+export const googleSpreadsheetCreds = {
+  type: TYPE,
+  project_id: PROJECT_ID,
+  private_key_id: PRIVATE_KEY_ID,
+  private_key: PRIVATE_KEY,
+  client_email: CLIENT_EMAIL,
+  client_id: CLIENT_ID,
+  auth_uri: AUTH_URI,
+  token_uri: TOKEN_URI,
+  auth_provider_x509_cert_url: AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: CLIENT_X509_CERT_URL,
+};
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -4,12 +4,31 @@ import { HttpException } from '@exceptions/HttpException';
 import { User } from '@interfaces/users.interface';
 import userModel from '@models/users.model';
 import { isEmpty } from '@utils/util';
+import { GoogleSpreadsheet } from 'google-spreadsheet';
+import { googleSpreadsheetCreds } from '@config';
 
 class UserService {
   public users = userModel;
 
   public async findAllUser(): Promise<User[]> {
-    const users: User[] = this.users;
+    const docId = '12-bJ9eUe9Q-EL71Rzai8KP1bgMurjpVpsuTQetNfQHQ';
+    const sheetId = 0;
+    const doc = new GoogleSpreadsheet(docId);
+    await doc.useServiceAccountAuth(googleSpreadsheetCreds);
+    await doc.loadInfo();
+    const sheet = doc.sheetsById[sheetId];
+    const rows = await sheet.getRows();
+
+    const users: User[] = [];
+
+    for (const row of rows) {
+      const data = row._rawData;
+      users.push({
+        id: parseInt(data[0], 10),
+        email: data[1],
+        password: data[2],
+      });
+    }
     return users;
   }
 
@@ -21,7 +40,7 @@ class UserService {
   }
 
   public async createUser(userData: CreateUserDto): Promise<User> {
-    if (isEmpty(userData)) throw new HttpException(400, "userData is empty");
+    if (isEmpty(userData)) throw new HttpException(400, 'userData is empty');
 
     const findUser: User = this.users.find(user => user.email === userData.email);
     if (findUser) throw new HttpException(409, `This email ${userData.email} already exists`);
@@ -34,7 +53,7 @@ class UserService {
   }
 
   public async updateUser(userId: number, userData: CreateUserDto): Promise<User[]> {
-    if (isEmpty(userData)) throw new HttpException(400, "userData is empty");
+    if (isEmpty(userData)) throw new HttpException(400, 'userData is empty');
 
     const findUser: User = this.users.find(user => user.id === userId);
     if (!findUser) throw new HttpException(409, "User doesn't exist");
